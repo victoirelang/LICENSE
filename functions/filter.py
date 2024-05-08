8d0fbf4912cbfa30dd366ad7d6abc5a1af3f78e5
victoirelang <victoirelng18@gmail.com>
2024-05-08 18:19:32 +0200

    changes

--- functions/filter.py
+++ functions/filter.py
@@ -1,4 +1,4 @@
- import pandas as pd
+import pandas as pd
 
 def filter_Isopropyl(database, column_name="Molecule", keyword="Isopropyl"):
     database_contains_Isopropyl= database[database[column_name].str.contains(keyword, na=False)]
