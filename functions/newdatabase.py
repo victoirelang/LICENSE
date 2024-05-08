0362e6a65cb78bad16e2c53d8e3ebdd7af73250e
victoirelang <victoirelng18@gmail.com>
2024-05-08 19:04:31 +0200

    changes

--- functions/newdatabase.py
+++ functions/newdatabase.py
@@ -5,7 +5,7 @@ def new_database_no_Isopropyl(database, column_name="Cream", excluded_value="La 
     return database_filtered_1
 
 
-def new_database_no_NaOH(database_filtered_1, column_name, excluded_values=["La Roche-Posay","Nivea","Avene","La Prairie"]:
+def new_database_no_NaOH(database_filtered_1, column_name, excluded_values=["La Roche-Posay","Nivea","Avene","La Prairie"]):
     database_filtered_2= database_filtered_1[database_filtered_1[column_name].isin(excluded_values)]
     return database_filtered_2
     
