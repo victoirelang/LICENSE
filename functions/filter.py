6ce2124d06ac24958728d538c7f1d60060e18765
victoirelang <victoirelng18@gmail.com>
2024-05-08 18:35:28 +0200

    xhanges

--- functions/filter.py
+++ functions/filter.py
@@ -4,5 +4,11 @@ def filter_Isopropyl(database, column_name="Molecule", keyword="Isopropyl"):
     database_contains_Isopropyl= database[database[column_name].str.contains(keyword, na=False)]
     return database_contains_Isopropyl
 
+def filter_NaOH(database_filtered_1, column_name="Molecule", keyword="Sodium Hydroxide"):
+    database_contains_NaOH= database_filtered_1[database_filtered_1[column_name].str.contains(keyword, na=False)]
+    return database_contains_NaOH
 
+def filter_allother(database, column_name="Molecule", keywords="Linalool|Citronellol|Limonene|Benzyl Alcohol|Benzyl Salicylate"):
+    database_filtered_2=database_filtered_1[database_filtered_1[column_name].str.contains(keywords, na=False)]
+    return database_filtered_2
 
