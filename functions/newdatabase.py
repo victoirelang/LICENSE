f43f22b9435c2db27b9c46859849b0ea72522288
victoirelang <victoirelng18@gmail.com>
2024-05-09 10:28:05 +0200

    2

--- functions/newdatabase.py
+++ functions/newdatabase.py
@@ -9,6 +9,6 @@ def new_database_no_NaOH(database, column_name="Cream", excluded_values=["La Roc
     database_filtered_2= database[~database[column_name].isin(excluded_values)]
     return database_filtered_2
 
-def new_database_no_other(database, column_name, excluded_values=["Nuxe","Diadermine","L'Oréal","La Neige"]):
+def new_database_no_other(database, column_name="Cream", excluded_values=["Nuxe","Diadermine","L'Oréal","La Neige"]):
     return pd.DataFrame([row for idx, row in database.iterrows() if row[column_name] not in excluded_values])
     
