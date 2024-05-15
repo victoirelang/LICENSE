9e2270328676cff0a7c1d9668a9aabc9291c0c58
victoirelang <victoirelng18@gmail.com>
2024-05-15 15:42:15 +0200

    functions

--- functions/ranking.py
+++ functions/ranking.py
@@ -4,8 +4,24 @@ def filter_pregnancy_risk(data):
     print("Data base with molecules dangerous for pregnant women")
     return dataPR
     
-def found_percentage_molecules_danger(dictionnaire_molecules, dictionnaire_riskymolecules):
-    percentage_molecules_danger = {}
+def def analyse_molecules(database):
+    
+    dataPR = data[data["Pregnancy Risk"] != "No"]
+    
+    nombre_molecules_par_creme = data.groupby('Cream')['Molecule'].nunique()
+    
+    dictionnaire_molecules = nombre_molecules_par_creme.to_dict()
+
+    nb_risky_molecules = dataPR.groupby('Cream')['Molecule'].nunique()
+
+    dictionnaire_riskymolecules = nb_risky_molecules.to_dict()
+
+    dictionnaire_complet = {cream: 0 for cream in data['Cream'].unique()}
+
+    dictionnaire_complet.update(dictionnaire_riskymolecules)
+
+    pourcentage_molecules_risquees = {}
+
     for creme, total in dictionnaire_molecules.items():
         if creme in dictionnaire_riskymolecules:
             risque = dictionnaire_riskymolecules[creme]
@@ -13,7 +29,14 @@ def found_percentage_molecules_danger(dictionnaire_molecules, dictionnaire_risky
             pourcentage_arrondi = round(pourcentage, 2)  
         else:
             pourcentage_arrondi = 0  
+
         
         pourcentage_molecules_risquees[creme] = f"{pourcentage_arrondi}%"
 
+    
+    print("Pourcentage de molécules risquées par crème:")
+    for creme, pourcentage in pourcentage_molecules_risquees.items():
+        print(f"{creme}: {pourcentage}")
+
     return pourcentage_molecules_risquees
+
