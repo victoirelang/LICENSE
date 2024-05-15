cff39dd996171156da07144d2d078b2a63136510
victoirelang <victoirelng18@gmail.com>
2024-05-15 15:24:19 +0200

    new function

--- functions/ranking.py
+++ functions/ranking.py
@@ -4,3 +4,16 @@ def filter_pregnancy_risk(data):
     print("Data base with molecules dangerous for pregnant women")
     return dataPR
     
+def found_percentage_molecules_danger(dictionnaire_molecules, dictionnaire_riskymolecules):
+    percentage_molecules_danger = {}
+    for creme, total in dictionnaire_molecules.items():
+        if creme in dictionnaire_riskymolecules:
+            risque = dictionnaire_riskymolecules[creme]
+            pourcentage = (risque / total) * 100
+            pourcentage_arrondi = round(pourcentage, 2)  
+        else:
+            pourcentage_arrondi = 0  
+        
+        pourcentage_molecules_risquees[creme] = f"{pourcentage_arrondi}%"
+
+    return pourcentage_molecules_risquees
