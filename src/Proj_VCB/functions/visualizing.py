33d248cac1c6c9bf634198039df2030f3095ebf1
victoirelang <victoirelng18@gmail.com>
2024-05-22 10:50:47 +0200

    new function

---
+++ src/Proj_VCB/functions/visualizing.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from rdkit import Chem
+from rdkit.Chem import Draw
+from IPython.display import display
+def visualize_molecules_for_cream(database, cream_name):
+    df = pd.read_csv(database, sep=';')
+    filtered_df = df[df['Cream'] == cream_name]
+    color_map = {
+        'Isopropyl': (0, 1, 0),  # Vert
+        'NaOH': (0, 0, 1),       # Bleu
+        'Linalool': (1, 0.5, 0), # Orange 1
+        'Citronellol': (1, 0.6, 0), # Orange 2
+        'Limonene': (1, 0.7, 0),   # Orange 3
+        'Benzyl Alcohol': (1, 0.8, 0), # Orange 4
+        'Benzyl Salicylate': (1, 0.9, 0) # Orange 5
+    }
+    smarts_patterns = {
+        'Isopropyl': 'CC(C)O',
+        'NaOH': '[Na+].[OH-]',
+        'Linalool': 'CC(C)CC1=CC=C(C=C1)O',
+        'Citronellol': 'CC(C)CCC(C)(C)CO',
+        'Limonene': 'CC1=CC=CCC1(C)C',
+        'Benzyl Alcohol': 'C1=CC=C(C=C1)CO',
+        'Benzyl Salicylate': 'C1=CC=C(C=C1)OC(=O)C2=CC=CC=C2'
+    }
+    for index, row in filtered_df.iterrows():
+        smi = row['Smiles']
+        mol = Chem.MolFromSmiles(smi)
+        highlight_dict = {}
+        for compound, smarts in smarts_patterns.items():
+            pattern = Chem.MolFromSmarts(smarts)
+            matches = mol.GetSubstructMatches(pattern)
+            for match in matches:
+                for idx in match:
+                    highlight_dict[idx] = color_map[compound]
+        if mol:
+            img_data = highlight_atoms(mol, highlight_dict)
+            display(Draw.MolToImage(mol, highlightAtoms=highlight_dict.keys(), highlightAtomColors=highlight_dict))
+            with open(f"molecule_{index}.png", "wb") as f:
+                f.write(img_data)
+
+
+    
