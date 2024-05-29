0d2cafa1b600f72f33a6d08a1d47999d42444c97
victoirelang <160872660+victoirelang@users.noreply.github.com>
2024-05-29 19:11:22 +0200

    Update __init__.py

--- src/Proj_VCB/__init__.py
+++ src/Proj_VCB/__init__.py
@@ -3,5 +3,6 @@
 from .functions import filter
 from .functions import visualizing
 from .functions import newdatabase
+from .functions import ranking_for_pregnancy
 
-__all__ = ['filter', 'visualizing','newdatabase']
+__all__ = ['filter', 'visualizing','newdatabase', 'ranking_for_pregnancy']
